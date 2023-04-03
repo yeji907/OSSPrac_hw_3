fb561bd660097e6351daf149d969affe22f6a091
yeji907 <yeji3665@dgu.ac.kr>
2023-04-03 14:20:40 +0900

    0403

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = str(input("이름을 입력하세요: "))
 num = str(input("학번을 입력하세요: "))
 dep = str(input("학과를 입력하세요: "))
+year = str(input("학년을 입력하세요: "))
 
 print("\n<출력>")
 print("이름:", name)
 print("학번:", num)
 print("학과:", dep)
+print("학년:", year)
